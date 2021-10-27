86ba595974ad9f60cb9d44a75394b02e97b2d696
harisantoso <harysanttos@gmail.com>
2021-10-27 16:23:35 +0700

    membuta struktur folder dan routing mvc

--- public/index.php
+++ public/index.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Belajar MVC PHP</title>
 </head>
+
 <body>
   <h3>Belajar Konsep MVC Pada pemrogramman PHP</h3>
   <p>Belajar Konsep MVC dengan pemrogramman php dan menarapkan pada GitHub</p>
-  <ul>
-    <li>1. MPC</li>
-  </ul>
 </body>
+
 </html>
+
+
+
+<?php
+
+require_once '../app/init.php';
+
+// panggil class dari app/core
+$app = new App;
+
+?>
