7e712c28e3b84a6c068a318c81b1cad678380636
harisantoso <harysanttos@gmail.com>
2021-11-04 19:49:50 +0700

    menambahkan insert dengan konsep mvc

--- app/views/mahasiswa/detail.php
+++ app/views/mahasiswa/detail.php
@@ -1,12 +1,15 @@
 <div class="container mt-5">
 
+  <h3 class="mt-3">Detail Mahasiswa</h3>
+
   <div class="card" style="width: 18rem;">
     <div class="card-body">
       <h5 class="card-title"><?= $data['mhs']['nama']; ?></h5>
-      <h6 class="card-subtitle mb-2 text-muted"><?= $data['mhs']['nik']; ?></h6>
+      <h6 class="card-subtitle mb-2 text-muted"><?= $data['mhs']['nim']; ?></h6>
       <p class="card-text"><?= $data['mhs']['email']; ?></p>
       <p class="card-text"><?= $data['mhs']['jurusan']; ?></p>
-      <a href="<?= BASEURL; ?>/mahasiswa" class="card-link">Back</a>
+      <!-- <a href="<?= BASEURL; ?>/mahasiswa" class="card-link text-decoration-none">Back</a> -->
+      <a href="<?= BASEURL; ?>/mahasiswa" class="badge bg-dark text-decoration-none">Back</a>
     </div>
   </div>
 
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,14 +1,19 @@
-<div class="container mt-5">
+<div class="container mt-4">
 
   <div class="row">
     <div class="col-6">
-      <h3>Daftar Mahasiswa</h3>
+
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#formModal">
+        Tambah Data Mahasiswa
+      </button>
+      <h3 class="mt-3">Daftar Mahasiswa</h3>
 
       <ul class="list-group">
         <?php foreach ($data['mhs'] as $mhs) : ?>
           <li class="list-group-item d-flex justify-content-between align-items-center">
             <?= $mhs['nama']; ?>
-            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge bg-secondary">Detail</a>
+            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge bg-dark text-decoration-none">Detail</a>
           </li>
         <?php endforeach; ?>
       </ul>
@@ -16,4 +21,52 @@
     </div>
   </div>
 
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+          <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama">
+          </div>
+
+          <div class="form-group">
+            <label for="nim">NIM</label>
+            <input type="number" class="form-control" id="nim" name="nim">
+          </div>
+
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email">
+          </div>
+
+          <div class="form-group">
+            <label for="jurusan">Jurusan</label>
+            <select class="form-control" id="jurusan" name="jurusan">
+              <option value="Management Informasi">Management Informasi</option>
+              <option value="Sistem Informasi">Sistem Informasi</option>
+              <option value="Jaringan">Jaringan</option>
+            </select>
+          </div>
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+
+        </form>
+
+      </div>
+    </div>
+  </div>
 </div>
